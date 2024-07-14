ea6a5f8508ee33bfb2919a84122fb479cd7c1d77
EgorDyach <tvoysosed33@gmail.com>
2024-07-14 23:46:17 +0300

    fix(main): added block if no robots

--- src/modules/shopPage/ShopPage.tsx
+++ src/modules/shopPage/ShopPage.tsx
@@ -1,19 +1,37 @@
 import ContentLoader from "@components/ContentLoader";
 import Flex from "@components/Flex";
-import { Header } from "@components/Typography";
+import { Header, Paragraph } from "@components/Typography";
 import { useEffect } from "react";
 import { ShopCard } from "./ShopCard";
 import { useAppDispatch } from "@hooks/useAppDispatch";
 import { fetchRobots } from "@store/ui/thunks";
 import { useSelector } from "react-redux";
-import { uiSelectors } from "@store/ui";
+import { uiActions, uiSelectors } from "@store/ui";
 import { ShopControls } from "./ShopControls";
+import img from "/img/notFound.png";
+import { content } from "@lib/theme/colors";
+import { indent } from "@lib/theme/sizes";
+import styled from "styled-components";
+import { getMaxPrice } from "./helpers";
+import Image from "@components/Image";
+
+const ClearFilters = styled.button`
+  background-color: ${content.primary};
+  border-radius: 10px;
+  border: none;
+  color: ${content.white};
+  padding: ${indent.medium} ${indent.vlarge};
+  cursor: pointer;
+  margin-top: ${indent.xlarge};
+  font-size: ${indent.medium};
+  font-family: "Montserrat";
+`;
 
 export const ShopPage = () => {
   const robots = useSelector(uiSelectors.getFiltered);
   const dispatch = useAppDispatch();
   const status = useSelector(uiSelectors.getRequests);
-
+  const allServerRobots = useSelector(uiSelectors.getRobots);
   useEffect(() => {
     dispatch(fetchRobots());
   }, [dispatch]);
@@ -25,11 +43,34 @@ export const ShopPage = () => {
       <Header>Роботы</Header>
       <Flex direction="column">
         <ShopControls />
-        <Flex $top="large" basis="50%" wrap="wrap" gap="16px">
-          {robots.map((robot) => (
-            <ShopCard robot={robot} />
-          ))}
-        </Flex>
+        {!!robots.length && (
+          <Flex $top="large" basis="50%" wrap="wrap" gap="16px">
+            {robots.map((robot) => (
+              <ShopCard robot={robot} />
+            ))}
+          </Flex>
+        )}
+        {!robots.length && (
+          <Flex align="center" direction="column">
+            <Flex direction="column">
+              <Image $maxWidth="300px" src={img} />
+              <Paragraph>По вашему запросу ничего не найдено!</Paragraph>
+              <ClearFilters
+                onClick={() =>
+                  dispatch(
+                    uiActions.setFilteredRobots({
+                      minPrice: 0,
+                      maxPrice: getMaxPrice(allServerRobots),
+                      activeSort: "POPULAR",
+                    }),
+                  )
+                }
+              >
+                Отчистить фильтры
+              </ClearFilters>
+            </Flex>
+          </Flex>
+        )}
       </Flex>
     </>
   );
